5c993ec167b8ab7fd7b23457f8dfd2a355501e5a
Austin Malmberg <austin.malmberg@gmail.com>
2018-08-22 15:47:44 -0400

    Created WordFunnel_Intermediate project

---
+++ DailyProgrammer_WordFunnel/src/com/austin/challenge366/funnels/WordFunnel_Intermediate.java
@@ -0,0 +1,21 @@
+package com.austin.challenge366.funnels;
+
+import java.io.IOException;
+
+import com.austin.challenge366.WordMap;
+
+public class WordFunnel_Intermediate extends WordFunnel {
+
+	public WordFunnel_Intermediate() throws IOException {
+		super();
+	}
+	
+	public WordFunnel_Intermediate(WordMap wordMap) {
+		super(wordMap);
+	}
+
+	@Override
+	public void printChallenges() {
+		
+	}
+}
